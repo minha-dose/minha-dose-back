b7dcf5d4cb63801b0d13afd301c09d7e09cdc7d7
leoaccioly05 <162645752+leoaccioly05@users.noreply.github.com>
2025-06-14 18:57:04 +0000

    feito segundo orientacoes dos integrantes responsaveis

--- src/main/java/com/minhadose/demo/repository/AppointmentRepository.java
+++ src/main/java/com/minhadose/demo/repository/AppointmentRepository.java
@@ -1,9 +1,12 @@
 package com.minhadose.demo.repository;
-
 import com.minhadose.demo.model.AppointmentModel;
+import com.minhadose.demo.model.UbsModel;
+import com.minhadose.demo.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<AppointmentModel, Long> {
+    AppointmentModel findByUser(UserModel user);
+    AppointmentModel findByUbs(UbsModel ubs);
 }
