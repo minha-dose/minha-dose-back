1e71ebacd505fae2ff384e942bbeb7465ce2ef51
leoaccioly05 <162645752+leoaccioly05@users.noreply.github.com>
2025-06-14 17:57:55 +0000

    commit de léo

---
+++ src/main/java/com/minhadose/demo/repository/AppointmentRepository.java
@@ -0,0 +1,9 @@
+package com.minhadose.demo.repository;
+
+import com.minhadose.demo.model.AppointmentModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<AppointmentModel, Long> {
+}
